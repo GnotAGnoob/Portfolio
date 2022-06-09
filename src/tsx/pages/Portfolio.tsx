4f4bbada755f7008a93a1ee9c4238fa22b297b89
GnotAGnoob <werewere@seznam.cz>
2022-06-09 20:14:48 +0200

    grid fixes

--- src/tsx/pages/Portfolio.tsx
+++ src/tsx/pages/Portfolio.tsx
@@ -9,7 +9,7 @@ export const Portfolio: FunctionComponent = () => {
 			<h1 className="max-w-none border-b-2 border-[color:var(--clr-primary-700)] pb-8 md:pb-8">
 				Daniel Svátek - Portfolio
 			</h1>
-			<div className="grid grid-cols-[repeat(auto-fit,minmax(33ch,1fr))] md:grid-cols-[repeat(auto-fit,minmax(38ch,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(40ch,1fr))] place-content-center gap-20 mt-12 md:mt-20 sm:px-5">
+			<div className="grid grid-cols-[repeat(auto-fit,minmax(33ch,1fr))] md:grid-cols-[repeat(auto-fit,minmax(40ch,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(42ch,1fr))] place-content-center gap-20 mt-12 md:mt-20 sm:px-5">
 				{DataPortfolio.map((item, index) => (
 					<CardPortfolio key={index} {...item} />
 				))}
